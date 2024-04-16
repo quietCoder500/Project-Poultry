24756321278b4647d7d524483d1e2fc9e20b300a
quietCoder500 <raspberrypisms1@gmail.com>
2024-04-16 17:07:33 -0400

    Add Password changing

--- backend/dashboard/admin.py
+++ backend/dashboard/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from unfold.admin import ModelAdmin
 
+from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+
 from .models import Family, Note, Tag, UserProfile
 
 
@@ -10,7 +12,9 @@ admin.site.unregister(User)
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin, ModelAdmin):
-    pass
+    form = UserChangeForm
+    add_form = UserCreationForm
+    change_password_form = AdminPasswordChangeForm
 
 
 @admin.register(Note)
